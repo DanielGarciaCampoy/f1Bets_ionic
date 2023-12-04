7a9e1cd295b1db4ca0b550804934ad5ad8fbece9
Daniel García Campoy <daniel.garcia.campoy@gmail.com>
2023-12-04 19:19:04 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'f1Bets',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
