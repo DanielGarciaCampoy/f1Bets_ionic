142261eae8fb8be94583323c4b8c51e6150c395f
Daniel García Campoy <daniel.garcia.campoy@gmail.com>
2024-01-18 15:42:38 +0100

    firebase añadido e imports del core.module ampliado

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    // components
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    IonicModule.forRoot(),
+    HttpClientModule,
+    ReactiveFormsModule
+    // translate
+  ],
+  exports:[
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    // components
   ]
 })
 export class CoreModule { }
