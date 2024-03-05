c9f3ef8012a7ba1b886b10d7c16d334082eb4a48
Daniel García Campoy <daniel.garcia.campoy@gmail.com>
2024-03-05 13:31:23 +0100

    Detalles de circuito añadido y traduccion de driver mejorada

--- src/app/pages/folder/circuitos/circuitos.page.ts
+++ src/app/pages/folder/circuitos/circuitos.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { AlertController, ModalController } from '@ionic/angular';
 import Circuit from 'src/app/core/interfaces/circuit.interface';
 import { CircuitsService } from 'src/app/core/services/circuits.service';
 import { ApuestaEditComponent } from '../../components/apuesta-edit/apuesta-edit.component';
@@ -13,17 +13,66 @@ import { ApuestasService } from 'src/app/core/services/apuestas.service';
 export class CircuitosPage implements OnInit {
 
   circuits: Circuit[] | undefined;
+  selectedCircuit: any;
+
+  isModalOpen = false;
+
+  setOpen(isOpen: boolean, circuit?: any) {
+    this.isModalOpen = isOpen;
+    if (circuit) {
+      this.selectedCircuit = circuit;
+    }
+  }
 
   constructor(
     private circuitsSvc: CircuitsService,
     private modalCtrl: ModalController,
-    private apuestasSvc: ApuestasService
+    private apuestasSvc: ApuestasService,
+    private alertCtrl: AlertController
   ) { }
 
   ngOnInit(): void {
     this.circuitsSvc.getCircuits().subscribe(circuits => {
       this.circuits = circuits;
     })
+  }
+
+  onEditCircuit(circuit: Circuit) {
+
+    this.setOpen(false);
+  }
+
+  onAddCircuit() {
+
+    this.setOpen(false);
+  }
+
+  async deleteCircuitAlert() {
+    const alert = await this.alertCtrl.create({
+      header: '¿Está seguro?',
+      message: 'Eliminarás este circuito permanentemente',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'rojo',
+        },
+        {
+          text: 'Aceptar',
+          handler: () => {
+            this.isModalOpen = false;
+            this.deleteCircuit(this.selectedCircuit);
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async deleteCircuit(circuit: Circuit) {
+    // const response = await this.circuitsSvc.deleteCircuit(circuit);
+    // console.log(response);
+    this.setOpen(false);
   }
 
 }
